a89661b571545192e5b9e9bb7488c5bd085f5bf9
504VIPER <ticklf7@gmail.com>
2022-07-08 23:37:07 -0300

    ajust

--- database/main.js
+++ database/main.js
@@ -1,13 +1,17 @@
 var db = openDatabase('DataBase', "2.0", "Mybase", 4048);
+
 db.transaction(function(criar){
     criar.executeSql("CREATE TABLE main (name TEXT, password TEXT)");
 });
 
 function salvar(){
+
     var name = document.getElementById('namedb').value;
+    
     var password = document.getElementById('passworddb').value;
 
     db.transaction(function(armazenar){
         armazenar.executeSql("INSERT INTO main (name, password) VALUES (?,?)",[name, password]);
     });
+
 };
--- storage/main.js
+++ storage/main.js
@@ -1,19 +1,22 @@
 function save_name(){
     localStorage.name = document.getElementById('name').value;
-}
+};
+
 
 function load_name(){
     if (localStorage.name){
         document.getElementById('name').value = localStorage.name;
-    }
-}
+    };
+};
+
 
 function save_password(){
     localStorage.password = document.getElementById('password').value;
-}
+};
+
 
 function load_password(){
     if (localStorage.senha){
         document.getElementById('password').value = localStorage.password;
-    }
-}
+    };
+};
